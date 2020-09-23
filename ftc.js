b7f5781bb0d688fbc657ae421aa5664297ad8815
413xPr06605 <70262451+413xPr06605@users.noreply.github.com>
2020-09-23 11:10:53 +0800

    Update ftc.js

--- ftc.js
+++ ftc.js
@@ -3,12 +3,7 @@ function changeBorder(){
 }
 
 function changing(){
-    setInterval('document.getElementById("mybd").style = "border-color: red;"',100)
-    setInterval('document.getElementById("mybd").style = "border-color: orange;"',200)
-    setInterval('document.getElementById("mybd").style = "border-color: yellow;"',300)
-    setInterval('document.getElementById("mybd").style = "border-color: green;"',400)
-    setInterval('document.getElementById("mybd").style = "border-color: cyan;"',500)
-    setInterval('document.getElementById("mybd").style = "border-color: blue;"',600)
-    setInterval('document.getElementById("mybd").style = "border-color: purple;"',700)
+    setInterval('document.getElementById("mybd").style = "border-color: cyan;"',100)
+    setInterval('document.getElementById("mybd").style = "border-color: blue;"',200)
 }
 changeBorder();
